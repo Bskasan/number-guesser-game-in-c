50deb2ee005e91f585c4241feae730d0a47a7faa
Bekir Kasan <b.kasan@hotmail.com>
2025-09-22 21:01:08 +0300

    started working on number guesser game

---
+++ NumberGuesser/NumberGuesser.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("Welcome to our new number guesser game...");
+	return 0;
+}
+
+/*
+Create a program called number guesser - Create your main method in C.
+Try to create creative UI on the Command Prompt...
+There will be secret number that si set at the start of the program without any user number.
+	Find a way to create random number in C programming when you run the program.
+	** IDEA ** -> You can generate power up numbers to get player more rights to guess.
+	** IDEA ** -> You can try to animate your text when game starts.
+The secret number should be picked from a certain range.
+The range of the number should be output to the player.
+The player should be prompted to enter his guess.
+The prompt should show how many guesses the player has left.
+The player will then enter his guess.
+If the guess is too high then the game will output that the guess was too high.
+If the guess is too low then the game will output that it was too low.
+The player only has a certain number of guesses, but it can change.
+The number of guesses should be based on the range of the secret number.
+Any wrong guess will decrement the number of guesses left.
+The game ends by saying what the secret number was regardless of if the player guessed the right number.
+When the game is over, the player should be prompted if he would like to play again.
+If yes, then the secret number should change.
+Any input errors by the player should have proper handling.
+Create level system, start with 1-5, 1-10, 1-20, 1-30, 1-40, 1-50, 1-100 (Final boss)
+Try to add timer, and show this timer when player is succeed or failed.
+*/
+
+
